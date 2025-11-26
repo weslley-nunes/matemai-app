4c91e1e0c4bcbc8c1e0281ba5b607c331b543b00
weslley-nunes <130309485+weslley-nunes@users.noreply.github.com>
2025-11-26 00:00:10 -0300

    Fix: Removendo argumento invalido experimental_allow_widgets do st.cache_resource

--- auth.py
+++ auth.py
@@ -138,7 +138,7 @@ import extra_streamlit_components as stx
 import hashlib
 import time
 
-@st.cache_resource(experimental_allow_widgets=True)
+@st.cache_resource
 def get_cookie_manager():
     return stx.CookieManager()
 
